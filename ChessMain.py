487129c2c066ce8dfdf051078671d9c2fdc62b6f
FireFrog-11 <isaac_w11@outlook.com>
2025-12-24 01:01:43 +1100

    chore: add game constants and imports

--- ChessMain.py
+++ ChessMain.py
@@ -1,3 +1,13 @@
 """
 This is our main driver file. It will be responsible for handling user input and displaying the current GameState object.
 """
+
+import pygame
+import ChessEngine
+
+# CONSTANTS
+WIDTH = HEIGHT = 512
+DIMENSION = 8 # board size is 8x8
+SQUARE_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15 # important for animations later on
+IMAGES = {}
