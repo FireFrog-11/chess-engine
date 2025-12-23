c9de9ea5f50c89c5d5495cdaf4a910d2bfb9b62d
FireFrog-11 <isaac_w11@outlook.com>
2025-12-24 00:40:08 +1100

    feat: add GameState class with initial board layout

--- ChessEngine.py
+++ ChessEngine.py
@@ -1,3 +1,19 @@
 """
 This class is responsible for storing all the information about the current state of a chess game. It will also be responsible for determining the valid moves at the current state. It will also keep a move log.
 """
+class GameState:
+    def __init__(self):
+        # board is a 8x8 2d list, each element of the list has 2 characters
+        # first character represents colour of piece, 'b' or 'w'
+        # second character represents type of piece, 'K', 'Q', 'B', 'N', 'R' or 'P'
+        # '--' represents an empty space
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
+        ]
