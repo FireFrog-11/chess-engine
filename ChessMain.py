c0e649bf2c80ba5f87c001414de2f86d3a02d1a4
FireFrog-11 <isaac_w11@outlook.com>
2025-12-24 01:09:10 +1100

    feat: load chess piece images into dictionary

--- ChessMain.py
+++ ChessMain.py
@@ -11,3 +11,13 @@ DIMENSION = 8 # board size is 8x8
 SQUARE_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 15 # important for animations later on
 IMAGES = {}
+
+"""
+Initialize a global dictionary of images. This will be called exactly once in main
+"""
+def load_images():
+    pieces = ["wP", "wR", "wN", "wB", "wQ", "wK", "bP", "bR", "bN", "bB", "bQ", "bK"]
+
+    for piece in pieces:
+        IMAGES[piece] = pygame.transform.scale(pygame.image.load("images/" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
+    # NOTE: we can access an image by saying 'IMAGES["wP"]'
